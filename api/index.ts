8b7af7ab95181311fa953f71dc053ae1543cd537
Walker <walker.b.lockard@gmail.com>
2024-11-08 16:31:01 -0600

    swing at fixing POST request issues

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import { handle } from "@hono/node-server/vercel";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import app from "../src/routes/app.routes.js";
 
+// I honestly don't know if this does anything...
 export const config = {
   api: {
     bodyParser: false,
